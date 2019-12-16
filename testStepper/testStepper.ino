36ba37a623f7721c28741e587c11c735ea0796bb
Josh Benson <josh.benson.805@gmail.com>
2019-12-16 09:38:04 -0800

    added testArduino file into github

--- testStepper/testStepper.ino
+++ testStepper/testStepper.ino
@@ -1,3 +1,6 @@
+#include "SpeedyStepper.h"
+SpeedyStepper stepper1;
+
 void setup() {
   Serial.begin(9600);
 
@@ -6,9 +9,13 @@ void setup() {
   stepper1.setAccelerationInStepsPerSecondPerSecond(500);
 
 }
-
+// lol this is to test git hub
 void loop() {
       int steps = 10
       stepper1.enableStepper();
-      stepper1.setupRelativeMoveInSteps(steps);
+      stepper1.setusetpRelativeMoveInSteps(steps);
+      while(!stepper1.motionComplete())
+     {
+        stepper1.processMovement();
+     }
 }
