0486328f9601f7813ebb92f96abb1a8ebf5bc2ef
Josh Benson <josh.benson.805@gmail.com>
2019-12-17 09:43:27 -0800

    fixed and ran test file

--- testStepper/testStepper.ino
+++ testStepper/testStepper.ino
@@ -1,18 +1,17 @@
 #include "SpeedyStepper.h"
-#include "SpeedyStepper.cpp"
 
 SpeedyStepper stepper1;
 
 void setup() {
   Serial.begin(9600);
 
-  stepper1.connectToPort(1);
+  stepper1.connectToPort(2);
   stepper1.setSpeedInStepsPerSecond(500);
   stepper1.setAccelerationInStepsPerSecondPerSecond(500);
 
 }
-// lol this is to test git hub
+
 void loop() {
-  stepper1.moveRelativeInSteps(-200);
+  stepper1.moveRelativeInSteps(-2000);
 
 }
