ea5ab440d516e84d52008419d291e74c92acdd43
pratikgupta <pratikg135@gmail.com>
2019-12-18 09:38:20 -0800

    led functions calls

--- main.py
+++ main.py
@@ -20,19 +20,19 @@ FORMAT_BYTE = 1
 FORMAT_STRING = 2
 NO_RESPONSE = 3
 
+
 def run_arduinos():
-    print(a.move_stepper([1, 1] + list((800).to_bytes(2, "little")), format_out=FORMAT_STRING))
-    time.sleep(3)
-    print(a.move_stepper([1, 0] + list((800).to_bytes(2, "little")), format_out=FORMAT_STRING))
-    time.sleep(3)
-    print(a.move_stepper([2, 0] + list((800).to_bytes(2, "little")), format_out=FORMAT_STRING))
-    time.sleep(3)
+    #    print(a.move_stepper([1, 1] + list((200).to_bytes(2, "little")), format_out=FORMAT_STRING))
+    #    time.sleep(3)
+    #    print(a.move_stepper([1, 0] + list((200).to_bytes(2, "little")), format_out=FORMAT_STRING))
+    #    time.sleep(3)
+    #    print(a.move_stepper([2, 0] + list((200).to_bytes(2, "little")), format_out=FORMAT_STRING))
+    #    time.sleep(3)
 
     print(a.blinkLED([23, 200] + list((800).to_bytes(2, "little")), format_out=FORMAT_STRING))
     time.sleep(.5)
     print(a.blinkLED([23, 200] + list((800).to_bytes(2, "little")), format_out=FORMAT_STRING))
     time.sleep(.5)
-
 
 
 run_arduinos()
