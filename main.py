9a6183d29a29e027b8c0d848b1c6d0db194aa595
Josh Benson <josh.benson.805@gmail.com>
2020-01-15 11:09:21 -0800

    test stepper 6

--- main.py
+++ main.py
@@ -24,21 +24,13 @@ NO_RESPONSE = 3
 def run_arduinos():
     print(a.setStepperSpeed([1, 1] + list((500).to_bytes(2, "little")), format_out=FORMAT_STRING))
 
-    #   print(a.moveStepper([1, 1] + list((200).to_bytes(2, "little")), format_out=FORMAT_STRING))
+       print(a.moveStepperToPos([1, 0] + list((200).to_bytes(2, "little"))+ list((800).to_bytes(2, "little")), format_out=FORMAT_STRING))
     #  time.sleep(.1)
     #  print(a.moveStepper([2, 1] + list((200).to_bytes(2, "little")), format_out=FORMAT_STRING))
     #   time.sleep(3)
     #   print(a.moveStepper([2, 0] + list((200).to_bytes(2, "little")), format_out=FORMAT_STRING))
     #   time.sleep(3)
 
-    #   time.sleep(1)
-
-    print(a.moveStepperDeg([2, 0] + list((360).to_bytes(2, "little")), format_out=FORMAT_STRING))
-
-    #  print(a.blinkLED([23, 200] + list((800).to_bytes(2, "little")), format_out=FORMAT_STRING))
-    #  time.sleep(.5)
-    #  print(a.blinkLED([23, 200] + list((800).to_bytes(2, "little")), format_out=FORMAT_STRING))
-    #  time.sleep(.5)
 
     time.sleep(5)
 
--- testStepper/testStepper.ino
+++ testStepper/testStepper.ino
@@ -2,10 +2,26 @@
 
 SpeedyStepper stepper1;
 SpeedyStepper stepper2;
+SpeedyStepper stepper3;
+SpeedyStepper stepper4;
+SpeedyStepper stepper5;
+SpeedyStepper stepper6;
 
 void setup() {
   Serial.begin(9600);
 
+  stepper1.connectToPort(1);
+  stepper1.setSpeedInStepsPerSecond(500);
+  stepper1.setAccelerationInStepsPerSecondPerSecond(500);
+  stepper2.connectToPort(2);
+  stepper2.setSpeedInStepsPerSecond(500);
+  stepper2.setAccelerationInStepsPerSecondPerSecond(500);
+  stepper1.connectToPort(1);
+  stepper1.setSpeedInStepsPerSecond(500);
+  stepper1.setAccelerationInStepsPerSecondPerSecond(500);
+  stepper2.connectToPort(2);
+  stepper2.setSpeedInStepsPerSecond(500);
+  stepper2.setAccelerationInStepsPerSecondPerSecond(500);
   stepper1.connectToPort(1);
   stepper1.setSpeedInStepsPerSecond(500);
   stepper1.setAccelerationInStepsPerSecondPerSecond(500);
@@ -27,10 +43,18 @@ void testStepper() {
   
   stepper1.setupRelativeMoveInSteps(1000);
   stepper2.setupRelativeMoveInSteps(1000);
-  while((!stepper1.motionComplete()) || (!stepper2.motionComplete()))
+  stepper3.setupRelativeMoveInSteps(1000);
+  stepper4.setupRelativeMoveInSteps(1000);
+  stepper5.setupRelativeMoveInSteps(1000);
+  stepper6.setupRelativeMoveInSteps(1000);
+  while((!stepper1.motionComplete()) || (!stepper2.motionComplete()) || (!stepper3.motionComplete()) || (!stepper4.motionComplete()) || (!stepper5.motionComplete()) || (!stepper6.motionComplete()))
  {
   stepper1.processMovement();
   stepper2.processMovement();
+  stepper3.processMovement();
+  stepper4.processMovement();
+  stepper5.processMovement();
+  stepper6.processMovement();
  }
 
 }
